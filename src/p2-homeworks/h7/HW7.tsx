0cfb8a7462f1484f80d628ae6169819753a33dc7
ssivenkov <ser.sivenkov@gmail.com>
2021-09-21 13:21:18 +0300

    homework 9

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
+import s from './HW7.module.css'
 
 const arr = ['x', 'y', 'z']
 
@@ -12,20 +13,22 @@ function HW7() {
             <hr/>
             homeworks 7
 
-            <div>
-                <SuperSelect
-                    options={arr}
-                    value={value}
-                    onChangeOption={onChangeOption}
-                />
-            </div>
-            <div>
-                <SuperRadio
-                    name={'radio'}
-                    options={arr}
-                    value={value}
-                    onChangeOption={onChangeOption}
-                />
+            <div className={s.container}>
+                <div>
+                    <SuperSelect
+                        options={arr}
+                        value={value}
+                        onChangeOption={onChangeOption}
+                    />
+                </div>
+                <div className={s.container}>
+                    <SuperRadio
+                        name={'radio'}
+                        options={arr}
+                        value={value}
+                        onChangeOption={onChangeOption}
+                    />
+                </div>
             </div>
 
             <hr/>
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,50 +1,60 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './Clock.module.css'
+import moment from "moment";
+import 'moment/locale/ru'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
-    const [show, setShow] = useState<boolean>(false)
+    const [time, setTime] = useState<string>('wait ...')
+    const [date, setDate] = useState<string>()
+    const [showTime, setShowTime] = useState<boolean>(false)
+    const [showDate, setShowDate] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearTimeout(timerId)
     }
     const start = () => {
         stop()
+        setShowTime(true)
         const id: number = window.setInterval(() => {
-            // setDate
+            let currentTime = moment().format('LTS')
+            let currentDate = moment().format('L');
+            setTime(currentTime)
+            setDate(currentDate)
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShowDate(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShowDate(false)
     }
-
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
 
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
+            {showTime && (
+                <div className={s.container}
+                     onMouseEnter={onMouseEnter}
+                     onMouseLeave={onMouseLeave}>
+                    <span className={s.text}
+                    >
+                    {time}
+                </span>
 
-            {show && (
-                <div>
-                    {stringDate}
+                    {showDate && (
+                        <span className={s.text}>
+                        {date}
+                    </span>
+                    )}
                 </div>
             )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+            <div className={s.buttonsContainer}>
+                <SuperButton className={s.button} onClick={start}>start</SuperButton>
+                <SuperButton className={s.button} onClick={stop}>stop</SuperButton>
+            </div>
         </div>
     )
 }
